0c1431590d5f57779a58f834e7739def97eb4e8a
subin1rai <bantawasubin@gmail.com>
2025-05-01 16:40:00 +0545

    backend login resiger

---
+++ BookLibrary/DTOs/Request/RegisterDTO.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookLibrary.DTOs.Request;
+
+
+public class RegisterDTO
+{
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+
